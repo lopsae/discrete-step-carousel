ab769d0ee03a2a470820c24eeac3561999f51756
Maic Lopez Saenz <maic@lopsae.com>
2025-11-16 19:02:53 -0800

    copy PrototypeSlider from old framework project

--- DiscreteStepSlider/Package.swift
+++ DiscreteStepSlider/Package.swift
@@ -1,10 +1,15 @@
 // swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+
 import PackageDescription
+
 
 let package = Package(
     name: "DiscreteStepSlider",
+    platforms: [
+        .iOS("26.0")
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
--- DiscreteStepSlider/Sources/DiscreteStepSlider/DiscreteStepSlider.swift
+++ DiscreteStepSlider/Sources/DiscreteStepSlider/DiscreteStepSlider.swift
@@ -1,2 +1,37 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  DiscreteStepSlider
+//  Created by Maic Lopez Saenz.
+//
+
+
+
+import SwiftUI
+
+
+struct PrototypeSlider: View {
+
+    var body: some View {
+        VStack {
+            Text("1.5").monospacedDigit()
+            Image(systemName: "arrowtriangle.down.fill")
+            HStack {
+                ForEach(0..<11) { index in
+                    GeometryReader { geometry in
+                        Path { path in
+                            let halfWidth = geometry.frame(in: .local).width / 2
+                            path.move(to: CGPoint(x: halfWidth, y: 0))
+                            path.addLine(to: CGPoint(x: halfWidth, y: 40))
+                        }
+                        .stroke(.gray, lineWidth: 2)
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+#Preview {
+    PrototypeSlider()
+}
