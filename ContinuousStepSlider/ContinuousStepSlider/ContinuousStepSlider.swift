5bbe7dda6d81501423a98c51da576c39061863d0
Maic Lopez Saenz <maic@lopsae.com>
2025-11-16 16:52:27 -0800

    add basic ui prototype

--- ContinuousStepSlider/ContinuousStepSlider/ContinuousStepSlider.swift
+++ ContinuousStepSlider/ContinuousStepSlider/ContinuousStepSlider.swift
@@ -1,9 +1,37 @@
 //
-//  ContinuousStepSlider.swift
 //  ContinuousStepSlider
-//
-//  Created by Maic Lopez Saenz on 2025-11-16.
+//  Created by Maic Lopez Saenz.
 //
 
-import Foundation
+
+import SwiftUI
+
+
+struct PrototypeSlider: View {
+
+    var body: some View {
+        VStack {
+            Text("1.5").monospacedDigit()
+            Image(systemName: "arrowtriangle.down.fill")
+            HStack {
+                ForEach(0..<11) { index in
+                    GeometryReader { geometry in
+                        Path { path in
+                            let halfWidth = geometry.frame(in: .local).width / 2
+                            path.move(to: CGPoint(x: halfWidth, y: 0))
+                            path.addLine(to: CGPoint(x: halfWidth, y: 40))
+                        }
+                        .stroke(.gray, lineWidth: 2)
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+
+#Preview {
+    PrototypeSlider()
+}
 
