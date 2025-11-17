7babdaa9c7aa6babd4774111f8a49b027b28bc83
Maic Lopez Saenz <maic@lopsae.com>
2025-11-16 20:22:34 -0800

    implement mock of PrototypeSlider

--- sources/DiscreteStepSlider.swift
+++ sources/DiscreteStepSlider.swift
@@ -4,13 +4,26 @@
 //
 
 
-
 import SwiftUI
 
 
 struct PrototypeSlider: View {
 
+    @Binding var selectedValue: Float
+
+    // Possible values for `selectedValue`
+    private(set) var values: [Float]
+    // Spacing between the mark for each value.
+    private(set) var spacing: CGFloat = 10
+
+
+    init(_ values: [Float], selectedValue: Binding<Float>) {
+        self.values = values
+        self._selectedValue = selectedValue
+    }
+
     var body: some View {
+        // TODO: this is a mockup view of the intended ui.
         VStack {
             Text("1.5").monospacedDigit()
             Image(systemName: "arrowtriangle.down.fill")
@@ -27,11 +40,16 @@ struct PrototypeSlider: View {
                 }
             }
         }
+        .task {
+            print(values)
+        }
     }
 
 }
 
 
 #Preview {
-    PrototypeSlider()
+    @Previewable @State var selectedValue: Float = 1.5
+    let values: [Float] = Array(stride(from: 0.0, to: 3.0, by: 0.1))
+    PrototypeSlider(values, selectedValue: $selectedValue)
 }
