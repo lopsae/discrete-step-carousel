2bbaa6c6b8a6f297c60f4bfd50fd6e46dc3d5456
Maic Lopez Saenz <maic@lopsae.com>
2025-11-19 17:27:59 -0800

    add reference link to package file

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// https://theswiftdev.com/the-swift-package-manifest-file/
+
 
 import PackageDescription
 
