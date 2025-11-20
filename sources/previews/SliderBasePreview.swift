6e0f97b78ed7c7ef9cec96f67410acb3c9417614
Maic Lopez Saenz <maic@lopsae.com>
2025-11-19 23:06:49 -0800

    move HistoricValue to its own file

--- sources/previews/SliderBasePreview.swift
+++ sources/previews/SliderBasePreview.swift
@@ -7,67 +7,6 @@
 import SwiftUI
 
 
-
-struct HistoricValue: View {
-
-    @State private var history: [Double] = []
-
-    let label: String
-    let value: Double
-
-    let historyToKeep: Int = 10
-
-
-    init(_ label: String, value: Double) {
-        self.value = value
-        self.label = label
-    }
-
-    var body: some View {
-        Text(value.formatted(.number.precision(.fractionLength(1))))
-            .monospacedDigit()
-            // Historic values placed in an overlay so that these never modify the size of the
-            // main text.
-            .overlay {
-                historicValues
-            }
-            .overlay(alignment: .leading) {
-                labelView
-            }
-            .onChange(of: value) { oldValue, newValue in
-                if history.count >= historyToKeep {
-                    history.removeLast(1 + history.count - historyToKeep)
-                }
-                history.insert(oldValue, at: 0)
-            }
-    }
-
-
-    @ViewBuilder
-    private var historicValues: some View {
-        ForEach(history.enumerated(), id: \.offset) { index, historicValue in
-            Text(historicValue.formatted(.number.precision(.fractionLength(1))))
-                .font(.caption)
-                .monospacedDigit()
-                .opacity(1.0 - (index.asDouble / historyToKeep.asDouble))
-                .offset(x: ((index.asDouble + 1.0) * 25.0) + 5.0)
-        }
-    }
-
-
-    private var labelView: some View {
-        Text(label)
-            .font(.caption)
-            .padding(.horizontal, 5)
-            .fixedSize()
-            .alignmentGuide(.leading) { dimensions in
-                dimensions[.trailing]
-            }
-    }
-
-}
-
-
 #Preview {
     @Previewable @State var sliderPosition: DiscreteStepSlider.Position = .init(
         values: Array(stride(from: 0.0, to: 3.01, by: 0.2)),
@@ -93,7 +32,7 @@ struct HistoricValue: View {
         }
 
     // Selected index display.
-    // TODO: 
+    // TODO:
 //    HistoricValue("index:", value: sliderPosition.selectedIndex)
     Text(sliderPosition.selectedIndex.description)
         .font(.caption)
