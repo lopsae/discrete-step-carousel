7761eda4691134c607e1bd29e265b1a06b83d28e
Maic Lopez Saenz <maic@lopsae.com>
2025-11-21 15:28:55 -0800

    add previews for HistoricValue

--- sources/previews/HistoricValue.swift
+++ sources/previews/HistoricValue.swift
@@ -21,6 +21,7 @@ where Formatter.FormatInput == Value, Formatter.FormatOutput == String
 
     private(set) var historyPadding: Double = 5.0
     private(set) var historySpacing: Double = 25.0
+    private(set) var historyEdge: Edge = .trailing
 
 
     init(label: String, value: Value, format formatter: Formatter?) {
@@ -51,10 +52,15 @@ where Formatter.FormatInput == Value, Formatter.FormatOutput == String
     }
 
 
-    func history(padding: Double? = nil, spacing: Double? = nil) -> Self {
+    func history(
+        padding: Double? = nil,
+        spacing: Double? = nil,
+        edge: Edge? = nil
+    ) -> Self {
         var copy = self
         if let padding { copy.historyPadding = padding }
         if let spacing { copy.historySpacing = spacing }
+        if let edge    { copy.historyEdge    = edge }
         return copy
     }
 
@@ -63,12 +69,19 @@ where Formatter.FormatInput == Value, Formatter.FormatOutput == String
     private var historicValues: some View {
         ForEach(history.enumerated(), id: \.offset) { index, historicValue in
             let valueString = formatter?.format(historicValue) ?? String(describing: historicValue)
+            let offsetValue = ((index.asDouble + 1.0) * historySpacing) + historyPadding
+            let offsetSize = switch historyEdge {
+            case .top:      CGSize(width: .zero, height: -offsetValue)
+            case .leading:  CGSize(width: -offsetValue, height: .zero)
+            case .bottom:   CGSize(width: .zero, height: offsetValue)
+            case .trailing: CGSize(width: offsetValue, height: .zero)
+            }
             Text(valueString)
                 .font(.caption)
                 .monospacedDigit()
                 .fixedSize()
                 .opacity(1.0 - (index.asDouble / historyToKeep.asDouble))
-                .offset(x: ((index.asDouble + 1.0) * historySpacing) + historyPadding)
+                .offset(offsetSize)
         }
     }
 
@@ -95,14 +108,97 @@ extension HistoricValue where Formatter == NeverFormatStyle<Value> {
 }
 
 
-// Dummy implementation of FormatStyle to provide HistoricValue initializers without format.
+// Implementation of FormatStyle to provide HistoricValue initializers without format.
 struct NeverFormatStyle<Input>: FormatStyle {
-    func format(_ value: Input) -> String { "" }
+    func format(_ value: Input) -> String { .init() }
 }
 
 
-// TODO: add preview, using a formatter
-// HistoricValue(
-//label: "value:",
-//value: sliderPosition.selectedValue,
-//format: FloatingPointFormatStyle.number.precision(.fractionLength(1)))
+// MARK: - Previews
+
+
+#Preview {
+
+    @Previewable @State var selectedIndex: Int = 0
+    @Previewable @State var historyEdge: Edge = .top
+    let values = String.natoPhoneticAlphabet
+    let selection = values[selectedIndex]
+    HistoricValue(label: "selected:", value: selection)
+        .history(padding: 10, spacing: 40, edge: historyEdge)
+
+    HStack {
+        Button("Previous", systemImage: "arrowshape.left") {
+            selectedIndex = (selectedIndex + values.count - 1) % values.count
+        }
+        .labelStyle(.iconOnly)
+        .buttonStyle(.borderedProminent)
+
+        Button("Next", systemImage: "arrowshape.right") {
+            selectedIndex = (selectedIndex + 1) % values.count
+        }
+        .labelStyle(.iconOnly)
+        .buttonStyle(.borderedProminent)
+    }
+
+    VStack {
+        Button("Top", systemImage: "arrowshape.up.fill") {
+            historyEdge = .top
+        }
+        .labelStyle(.iconOnly)
+        .buttonStyle(.borderedProminent)
+
+        HStack {
+            Button("Leading", systemImage: "arrowshape.left.fill") {
+                historyEdge = .leading
+            }
+            .labelStyle(.iconOnly)
+            .buttonStyle(.borderedProminent)
+
+            Button("Bottom", systemImage: "arrowshape.down.fill") {
+                historyEdge = .bottom
+            }
+            .labelStyle(.iconOnly)
+            .buttonStyle(.borderedProminent)
+
+            Button("Trailing", systemImage: "arrowshape.right.fill") {
+                historyEdge = .trailing
+            }
+            .labelStyle(.iconOnly)
+            .buttonStyle(.borderedProminent)
+        }
+    }
+
+}
+
+
+#Preview("Formatted") {
+
+    @Previewable @State var value: Double = 0.12345
+    var step: Double = 0.12345
+
+    HistoricValue(
+        label: "value:",
+        value: value,
+        format: FloatingPointFormatStyle.number.precision(.fractionLength(2))
+    )
+    .history(spacing: 35)
+
+    Text("raw: \(value)")
+        .font(.caption)
+        .monospacedDigit()
+
+    HStack {
+        Button("Add", systemImage: "plus") {
+            value += step
+        }
+        .labelStyle(.iconOnly)
+        .buttonStyle(.borderedProminent)
+
+        Button("Substract", systemImage: "minus") {
+            value -= step
+        }
+        .labelStyle(.iconOnly)
+        .buttonStyle(.borderedProminent)
+    }
+
+}
