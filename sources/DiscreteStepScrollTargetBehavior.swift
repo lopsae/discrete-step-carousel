74bafb550deb82b6336c49af04042ca1b82271a7
Maic Lopez Saenz <maic@lopsae.com>
2025-12-09 13:23:20 -0800

    fix and add previews to DiscreteStepScrollTargetBehavior

--- sources/DiscreteStepScrollTargetBehavior.swift
+++ sources/DiscreteStepScrollTargetBehavior.swift
@@ -7,48 +7,124 @@
 import SwiftUI
 
 
+/// Defines a scroll behaviour where the horizontal target allways snaps to the closest multiple of
+/// a value.
 struct DiscreteStepScrollTargetBehavior: ScrollTargetBehavior {
     let step: Double
 
     func updateTarget(_ target: inout ScrollTarget, context: TargetContext) {
         let targetX = target.rect.origin.x
-        target.rect.origin.x = round(targetX / step) * step
+        target.rect.origin.x = (targetX / step).rounded(.toNearestOrEven) * step
     }
 }
 
 
-#Preview {
-    let gradient = LinearGradient(
-        colors: [.orange, .yellow],
+// MARK: - Previews.
+
+
+private struct PreviewContent {
+
+    static let gradient = LinearGradient(
+        colors: [.orange, .orange.opacity(0.5)],
         startPoint: .leading,
         endPoint: .trailing)
-    let spacing: CGFloat = 30
+
+}
+
+
+#Preview("Default", traits: .fixedHeader) {
+    let step: Double = 42
+
+    VStack {
+        Text("Items with step width will align to leading.")
+        Text("Last item might not display fully.")
+    }
+    .padding(.bottom)
+
+    Image(systemName: "arrowtriangle.down.fill")
+        .maxWidthFrame(alignment: .leading)
+
+    GeometryReader { geometry in
+        ScrollView(.horizontal) {
+            HStack(spacing: 0) {
+                ForEach(0..<15) { index in
+                    Rectangle()
+                        .fill(PreviewContent.gradient)
+                        .frame(width: step)
+                }
+            }
+        } // ScrollView
+        .scrollTargetBehavior(
+            DiscreteStepScrollTargetBehavior(step: step)
+        )
+    } // GeometryReader
+    .frame(height: 100)
+}
+
+
+#Preview("Spacers", traits: .fixedHeader) {
+    let step: Double = 50
+
+    Text("Using spacers to center the items.")
+        .padding(.bottom)
 
     Image(systemName: "arrowtriangle.down.fill")
 
     GeometryReader { geometry in
         ScrollView(.horizontal) {
             HStack(spacing: 0) {
-                // Leading spacer to center first item
+                // Leading spacer to center first item.
                 Color.teal
-                    .frame(width: (geometry.size.width - spacing) / 2)
+                    .frame(width: (geometry.size.width - step) / 2.0)
                     .opacity(0.2)
 
                 ForEach(0..<10) { index in
                     Rectangle()
-                        .fill(gradient)
-                        .frame(width: spacing)
+                        .fill(PreviewContent.gradient)
+                        .frame(width: step)
                 }
 
-                // Trailing spacer to center last item
+                // Trailing spacer to center last item.
                 Color.teal
-                    .frame(width: (geometry.size.width - spacing) / 2)
+                    .frame(width: (geometry.size.width - step) / 2.0)
                     .opacity(0.2)
             }
         } // ScrollView
         .scrollTargetBehavior(
-            DiscreteStepScrollTargetBehavior(step: spacing)
+            DiscreteStepScrollTargetBehavior(step: step)
         )
-        .frame(height: 100)
+
     } // GeometryReader
+    .frame(height: 100)
 }
+
+
+#Preview("Content Margins", traits: .fixedHeader) {
+    let step: Double = 50
+
+    Text("Using content margins to center the items.")
+        .padding(.bottom)
+
+    Image(systemName: "arrowtriangle.down.fill")
+
+    GeometryReader { geometry in
+        ScrollView(.horizontal) {
+            HStack(spacing: 0) {
+                ForEach(0..<10) { index in
+                    Rectangle()
+                        .fill(PreviewContent.gradient)
+                        .frame(width: step)
+                }
+            }
+        } // ScrollView
+        .contentMargins(
+            .horizontal((geometry.size.width - step) / 2.0),
+            for: .scrollContent)
+        .scrollTargetBehavior(
+            DiscreteStepScrollTargetBehavior(step: step)
+        )
+
+    } // GeometryReader
+    .frame(height: 100)
+}
+
