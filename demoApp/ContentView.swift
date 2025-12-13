61d8640510b677f530e1afead07359f81804d7e9
Maic Lopez Saenz <maic@lopsae.com>
2025-12-13 14:40:18 -0800

    add demo app project

---
+++ demoApp/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  DiscreteStepSliderDemo
+//  Created by Maic Lopez Saenz.
+//
+
+import SwiftUI
+
+import DiscreteStepSlider
+import PreviewUtilities
+
+
+struct ContentView: View {
+    @State var sliderPosition: DiscreteStepSliderPosition = .init(
+        values: String.alphabet.map(\.localizedUppercase),
+        selectedValue: "D")
+
+    var body: some View {
+        Image(systemName: "arrowtriangle.down.fill")
+            .font(.caption)
+
+        DiscreteStepSlider(position: $sliderPosition)
+        .frame(height: 44)
+
+        Text(sliderPosition.selectedValue)
+    }
+
+
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoApp/DiscreteStepSliderDemoApp.swift
@@ -0,0 +1,15 @@
+//
+//  DiscreteStepSliderDemo
+//  Created by Maic Lopez Saenz.
+//
+
+import SwiftUI
+
+@main
+struct DiscreteStepSliderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
