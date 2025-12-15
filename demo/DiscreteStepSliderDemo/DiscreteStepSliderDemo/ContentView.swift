62dff141910bbe34149ca10454558c891640b7dc
Maic Lopez Saenz <maic@lopsae.com>
2025-12-14 20:20:24 -0800

    add pristine xcode demo project

---
+++ demo/DiscreteStepSliderDemo/DiscreteStepSliderDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  DiscreteStepSliderDemo
+//  Created by Maic Lopez Saenz.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("DiscreteStepSlider Demo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
