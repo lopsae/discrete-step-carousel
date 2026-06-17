d141dff2057e9b7b80181d05b843457c08b33829
Maic Lopez Saenz <maic@lopsae.com>
2026-06-17 16:03:41 -0700

    update PreviewUtilities with feature branch

--- Package.swift
+++ Package.swift
@@ -17,8 +17,9 @@ let package = Package(
         ),
     ],
     dependencies: [
-//        .package(url: "https://github.com/lopsae/preview-utilities.git", "0.3.0"..<"1.0.0")
-        .package(path: "../preview-utilities"),
+//        .package(url: "https://github.com/lopsae/preview-utilities.git", "0.3.0"..<"1.0.0"),
+        .package(url: "https://github.com/lopsae/preview-utilities.git", branch: "feature/documentation-second-batch"),
+//        .package(path: "../preview-utilities"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0")
     ],
     targets: [
