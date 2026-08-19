9f8e11269ec977437e59dce1e869e2b7de08c7d3
Maic Lopez Saenz <maic@lopsae.com>
2026-08-19 08:02:24 -0700

    render illustrations through the demo app

---
+++ demo-app/illustrations/Illustrations.swift
@@ -0,0 +1,55 @@
+//
+//  Illustrations
+//  Created by Maic Lopez Saenz.
+//
+
+
+import DiscreteStepCarousel
+
+import PreviewUtilities
+import SwiftUI
+import Testing
+
+
+@MainActor
+struct BaseIllustrations {
+
+    let storage: IllustrationStorage
+
+    init() throws {
+        self.storage = try .init(
+            filePath: #filePath,
+            droppingComponents: 3, // filename, illustrations, demo-app.
+            appendingComponents: ["sources", "documentation.docc", "resources"]
+        ) {
+            // onImageStored
+            cgImage, filename in
+            Attachment.record(cgImage, named: filename, as: .png)
+        }
+    }
+
+
+    struct StepCarouselWithDefaultMark: View {
+        @State var position = StepCarouselPosition(
+            values: ["P", "Q", "R", "S", "T", "U", "V"],
+            selectedValue: "S"
+        )
+        var body: some View {
+            Text(position.selectedValue)
+            StepCarousel(position: $position)
+                .frame(height: StepCarouselDefaults.markHeight)
+            Text(position.selectedIndex.description)
+                .font(.caption)
+        }
+    }
+
+
+    @Test func testIllustration() throws {
+        try storage.renderAndStore("step-carousel", "with-default-mark", strategy: .windowHierarchy) {
+            DocumentationIllustration(sizing: .regular) {
+                StepCarouselWithDefaultMark()
+            }
+        }
+    }
+
+}
